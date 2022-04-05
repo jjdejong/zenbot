0c3bd7d629e4708446ef0c92fd52b359a34c716d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-05 16:49:16 +0800

    Delete post_install.js

--- post_install.js
+++
@@ -1,6 +0,0 @@
-var shell = require('shelljs')
-
-console.log('bundling WebApp components')
-shell.exec('webpack --mode production')
-console.log('installing genetic_backtester components')
-shell.exec('(cd scripts/genetic_backtester/ && npm i)')
